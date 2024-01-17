da9f151f180163bc0fc5c2d48196e598804d5cdc
Hoang Hy <haonghy01122003@gmail.com>
2024-01-17 23:05:48 +0700

    update

--- src/components/ImageStorage.tsx
+++ src/components/ImageStorage.tsx
@@ -1,13 +1,18 @@
 import { storage } from "@/services/firebase";
-import { getDownloadURL, ref } from "firebase/storage";
+import { getDownloadURL, getMetadata, ref } from "firebase/storage";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Video from "next-video";
 
 export default function ImageStorage({ src }: { src: string }) {
 	const [url, setUrl] = useState<string>();
+	const [type, setType] = useState<string>();
 	const [isSeleted, setIsSelected] = useState<boolean>(false);
 
 	useEffect(() => {
+		getMetadata(ref(storage, src)).then((metadata) => {
+			setType(metadata.contentType?.split("/").at(0));
+		});
 		getDownloadURL(ref(storage, src))
 			.then((url) => {
 				setUrl(url);
@@ -32,16 +37,22 @@ export default function ImageStorage({ src }: { src: string }) {
 			});
 	}, []);
 
-	return url ? (
+	return url && type ? (
 		<>
-			<Image
-				onClick={() => setIsSelected(true)}
-				className=" rounded-lg cursor-pointer"
-				src={url}
-				layout="fill"
-				objectFit="cover"
-				alt="image"
-			/>
+			{type == "video" ? (
+				<video  autoPlay loop>
+					<source src={url} type="video/mp4" />
+				</video>
+			) : (
+				<Image
+					onClick={() => setIsSelected(true)}
+					className=" rounded-lg cursor-pointer"
+					src={url}
+					layout="fill"
+					objectFit="cover"
+					alt="image"
+				/>
+			)}
 		</>
 	) : null;
 }
